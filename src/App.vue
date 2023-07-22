421aa8f8d9c11193f6b2a9ad228fb60e87a6c709
palashkumardas <info.palash27@gmail.com>
2023-07-22 12:01:16 +0600

    style binding

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 
 <script setup>
-import classBinding from './components/classBinding.vue';
+import styleBinding from './components/styleBinding.vue';
 </script>
 
 <template>
-<classBinding></classBinding>
+<styleBinding></styleBinding>
 </template>
 
 <style scoped>
--- src/components/styleBinding.vue
+++ src/components/styleBinding.vue
@@ -1,29 +1,49 @@
 <script setup>
- 
+    const bgOrange = 'background-color: rgba(251, 146, 60, 1)'
+    const bgIndigo = 'background-color: rgba(129, 140, 248, 1)'
+    const bgGreen = 'background-color: rgba(52, 211, 153, 1)'
+    const bgRed = 'background-color: rgba(220, 38, 38, 1)'
+    const bgDarkGreen = 'background-color: rgba(5, 150, 105, 1)'
+    const colors = {
+    borderColor: 'rgba(20, 184, 166, 1)', //rgba(239, 68, 68,1)
+    // 'border-color': 'rgba(20, 184, 166, 1)', //rgba(239, 68, 68,1)
+    color: 'rgba(19, 78, 74, 1)', //rgba(127, 29, 29,1)
+    backgroundColor: 'rgba(204, 251, 241, 1)' //rgba(254, 226, 226,1)
+    }
+    const weekday = true
+
+    const messageType = 'error'
+
+        if('error'==messageType){
+        colors.borderColor='rgba(239, 68, 68,1)'
+        colors.backgroundColor = 'rgba(254, 226, 226,1)'
+        colors.color = 'rgba(127, 29, 29,1)'
+    }
 </script>
 
 <template>
-   <section class="container mx-auto">
+    <section class="container mx-auto">
         <h1 class="text-center text-2xl py-10">Class & Style Binding</h1>
         <section class="flex space-x-10">
-            <div class="w-1/3  h-16" :class="bgOrange"></div>
-            <div class="w-1/3  h-16" :class="bgIndigo"></div>
-            <div class="w-1/3  h-16" :class="bgGreen"></div>
+            <div class="w-1/3  h-16" :style="bgOrange"></div>
+            <div class="w-1/3  h-16" :style="bgIndigo"></div>
+            <div class="w-1/3  h-16" :style="bgGreen"></div>
         </section>
-        <section class="mt-10">
-            <div class="h-16 bg-red-600 flex items-center justify-center text-white">
+     <section class="mt-10">
+            <div class="h-16 flex items-center justify-center text-white" :style="weekday? bgRed: bgDarkGreen">
                 It's a weekday!
             </div>
-        </section>
-        <section class="mt-10 hidden">
-            <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+        </section> 
+      <section class="mt-10">
+            <div class=" border-t-4  rounded-b  px-4 py-3 shadow-md" :style="colors">
                 <p class="font-bold">Our privacy policy has changed</p>
                 <p class="text-sm">Make sure you know how these changes affect you.</p>
             </div>
-        </section>
+        </section> 
     </section>
 </template>
 
-<style  scoped>
+
+<style scoped>
 
 </style>
