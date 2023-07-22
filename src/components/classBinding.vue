69db9358931d82cb3952cd0bc49a378da8c401c8
palashkumardas <info.palash27@gmail.com>
2023-07-22 11:17:35 +0600

    class binding done

--- src/components/classBinding.vue
+++ src/components/classBinding.vue
@@ -2,6 +2,24 @@
  const bgOrange="bg-orange-400";
  const bgIndigo="bg-red-400";
  const bgGreen="bg-purple-400";
+ const weekday= true;
+const color={
+    bgColor:'bg-teal-100',
+    borderColor:'border-teal-500',
+    textColor:'text-teal-900'
+}
+const messageType='warning';
+
+if(messageType=='error'){
+    color.bgColor='bg-red-100',
+    color.borderColor='border-red-500',
+    color.textColor='text-red-900'
+}
+if(messageType=='warning'){
+    color.bgColor='bg-orange-100',
+    color.borderColor='border-orange-500',
+    color.textColor='text-orange-900'
+}
 </script>
 
 <template>
@@ -12,13 +30,13 @@
             <div class="w-1/3  h-16" :class="bgIndigo"></div>
             <div class="w-1/3  h-16" :class="bgGreen"></div>
         </section>
-        <section class="mt-10 hidden">
-            <div class="bg-red-600 h-16 flex items-center justify-center text-white">
+        <section class="mt-10 ">
+            <div class="h-16 flex items-center justify-center text-white" :class="[weekday?'bg-red-600':'bg-green-600',weekday==true?'text-2xl':'text-4xl']">
                 It's a weekday!
             </div>
         </section>
-        <section class="mt-10 hidden">
-            <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+        <section class="mt-10">
+            <div class=" border-t-4  rounded-b  px-4 py-3 shadow-md" :class="[color.bgColor,color.borderColor,color.textColor]">
                 <p class="font-bold">Our privacy policy has changed</p>
                 <p class="text-sm">Make sure you know how these changes affect you.</p>
             </div>
---
+++ src/components/styleBinding.vue
@@ -0,0 +1,29 @@
+<script setup>
+ 
+</script>
+
+<template>
+   <section class="container mx-auto">
+        <h1 class="text-center text-2xl py-10">Class & Style Binding</h1>
+        <section class="flex space-x-10">
+            <div class="w-1/3  h-16" :class="bgOrange"></div>
+            <div class="w-1/3  h-16" :class="bgIndigo"></div>
+            <div class="w-1/3  h-16" :class="bgGreen"></div>
+        </section>
+        <section class="mt-10">
+            <div class="h-16 bg-red-600 flex items-center justify-center text-white">
+                It's a weekday!
+            </div>
+        </section>
+        <section class="mt-10 hidden">
+            <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+                <p class="font-bold">Our privacy policy has changed</p>
+                <p class="text-sm">Make sure you know how these changes affect you.</p>
+            </div>
+        </section>
+    </section>
+</template>
+
+<style  scoped>
+
+</style>
