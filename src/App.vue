5fe6e5d743bf09bb5855fc8122e0f0f498b525b2
palashkumardas <info.palash27@gmail.com>
2023-07-22 10:43:10 +0600

    class & style binding

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,13 @@
+
 <script setup>
-const location = "Dhaka";
-const weather ="r";
+import classBinding from './components/classBinding.vue';
 </script>
 
 <template>
-<section class="container mx-auto flex items-center flex-col">
-        <h1 class="text-center text-2xl py-10">Logic</h1>
-        <div class="w-1/3 bg-gray-200 p-10 flex items-center flex-col space-y-10">
-            <h1 class="text-3xl">{{ location }}</h1>
-            <template v-if="weather=='s'">
-            <h2 class="text-2xl" >Weather: Sunny</h2>
-            <img  src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911203/weather-icon-md.png" alt="">
-            </template>
-            <template v-if="weather=='pc'">
-            <h2 class="text-2xl" >Weather: Partly Cloudy</h2>
-            <img  src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912718/weather-icon-md.png" alt="">
-            </template>
-            <template v-if="weather=='r'">
-            <h2 class="text-2xl" >Weather: Rainy</h2>
-            <img  src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7913380/weather-icon-md.png" alt="">
-            </template>
-            <template v-if="weather=='t'">
-            <h2 class="text-2xl" >Weather: Thunderstorm</h2>
-            <img  src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912589/weather-icon-md.png" alt="">
-            </template>
-        </div>
-    </section>
+<classBinding></classBinding>
 </template>
 
 <style scoped>
 
 </style>
+
